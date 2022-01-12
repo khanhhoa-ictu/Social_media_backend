5c07fe930fbeca80319226441ffd108ddaf850af
HoaLK <khanhhoa01ictu@gmail.com>
2022-01-12 13:59:27 +0700

    add API

--- api/routers/user.router.ts
+++ api/routers/user.router.ts
@@ -1,5 +1,6 @@
 export {};
 const user_controller = require('../controllers/user.controller');
+const auth = require('../utils/auth');
 
 module.exports = (app:any) => {
     app.route('/user/register')
@@ -11,18 +12,33 @@ module.exports = (app:any) => {
     app.route('/user/login')
     .post(user_controller.login);
 
-    // app.route('/user/request/forgotpassword/:email')
-    // .get(user_controller.requestForgotPassword)
+    app.route('/user/request/forgotpassword/:email')
+    .get(user_controller.requestForgotPassword)
 
-    // app.route('/user/verify/forgotpassword')
-    // .post(user_controller.verifyForgotPassword)
+    app.route('/user/verify/forgotpassword')
+    .post(user_controller.verifyForgotPassword)
 
-    // app.route('/user/forgotpassword')
-    // .post(user_controller.forgotPassword)
+    app.route('/user/forgotpassword')
+    .post(user_controller.forgotPassword)
+
+    app.route('/auth')
+    .post(auth.verify)
 
     app.route('/user/updateinfor')
     .post(user_controller.updateInfor)
 
     app.route('/user/updatepassword')
     .post(user_controller.updatePassword)
+
+    app.route('/user/delete')
+    .post(user_controller.deleteUser)
+
+    app.route('/user/:name/follower')
+    .post(user_controller.followerUser)
+
+    app.route('/user/friendsuggestion/:userId')
+    .get(user_controller.getFriendSuggestion)
+
+    app.route('/user/getuser')
+    .post(user_controller.getUser)
 }
