1696935637e735150fe3872abc244f29a4c0dbe7
HoaLK <khanhhoa01ictu@gmail.com>
2022-02-28 10:10:21 +0700

    fix database

--- index.ts
+++ index.ts
@@ -10,18 +10,17 @@ const postRouter = require('./api/routers/post.router');
 const conversationRouter = require('./api/routers/converstation.router')
 const messageRouter = require('./api/routers/messages.router')
 mongoose.Promise = global.Promise;
-
-const connect = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost/margatsni_db', {
+const URL = 'mongodb+srv://admin:aGsnda87yYPgKEbr@cluster0.io9hj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+ mongoose.connect(URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
-    } catch (err) {
-        console.log(err);
-    }
-}
-connect();
+        })
+        .then(()=>{
+            console.log('connect to MongoDB');
+            app.listen(port, () => console.log("server running on port " + port));
+
+        }).catch((err:string) => console.log(err))
+
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
@@ -32,5 +31,3 @@ postRouter(app);
 conversationRouter(app)
 messageRouter(app)
 app.get('/', (req: Request, res: Response) => { res.send('welcome to Test') })
-
-app.listen(port, () => console.log("server running on port " + port));
